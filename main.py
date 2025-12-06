83cf99a9b083c94139e8c873a9838be0538c3ed9
Meme <memekakati@gmail.com>
2025-12-05 21:44:08 -0500

    number guessing game

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-# importing neessary libraries
-import os
-import sys
+# importing library
+import random
+
+# random number variable
+random_number = random.randint(1, 50)
+
+# user input for guess
+user_input = int(input("Enter your guess: "))
+
+# counting numner of guesses
+guess_count = 0
+
+# while loop to keep the game going until the user guesses correctly
+while user_input != random_number:
+    guess_count += 1
+
+    if user_input < random_number:
+        print("Your guess is too low.")
+    else:
+        print("Your guess is too high")
+    user_input = int(input("Enter a new guess: "))
+
+    
