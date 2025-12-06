a56c8c587204ad4b233ae692604b06b15236ed4e
Meme <memekakati@gmail.com>
2025-12-05 23:54:43 -0500

    updating logic

--- main.py
+++ main.py
@@ -4,24 +4,26 @@ import random
 # random number variable
 random_number = random.randint(1, 50)
 
-# user input for guess
-user_input = int(input("Enter your guess: "))
-
 # counting numner of guesses
 guess_count = 0
+
+# max number of attempts
+max_attempts = 10
+
+# user input for guess
+user_input = int(input("Enter your guess: "))
 
 # while loop to keep the game going until the user guesses correctly
 while user_input != random_number:
     guess_count += 1
-    if user_input < random_number:
+    if user_input != random_number and guess_count < max_attempts:
         print("Your guess is too low.")
     else:
         print("Your guess is too high")
     user_input = int(input("Enter a new guess: "))
-
 guess_count += 1
-print(f"Congratulations! You guessed the number {random_number} correctly in {guess_count} guesses! ")
 
+print(f"Congratulations! You guessed the number {random_number} correctly in {guess_count} guesses! ")
 
     
 
