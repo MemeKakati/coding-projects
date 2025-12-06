090166b54dff19cf4c41a9bb4589220094717ca0
Meme <memekakati@gmail.com>
2025-12-05 21:57:57 -0500

    Updated if else statement and added guess count after to count correct guess as an attempt

--- main.py
+++ main.py
@@ -13,11 +13,15 @@ guess_count = 0
 # while loop to keep the game going until the user guesses correctly
 while user_input != random_number:
     guess_count += 1
-
     if user_input < random_number:
         print("Your guess is too low.")
     else:
         print("Your guess is too high")
     user_input = int(input("Enter a new guess: "))
 
+guess_count += 1
+print(f"Congratulations! You guessed the number {random_number} correctly in {guess_count} guesses! ")
+
+
     
+
