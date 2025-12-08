1c5a65c7451ea66826c4611af29c57864f98d246
Meme <memekakati@gmail.com>
2025-12-07 23:56:13 -0500

    fixed the logic and simplified the code to make sense

--- pass_gen.py
+++ pass_gen.py
@@ -4,20 +4,14 @@ letters = "abcdefghijklmnopqrstuvwxyz"
 numbers = "0123456789"
 symbols = "!@#$%^&*"
 
-# pick random characters
-random_letter = random.choice(letters)
-random_numbers = random.choice(numbers)
-random_symbols = random.choice(symbols)
-
 # user input on password length
 pass_len = int(input("How long do you want your password? "))
 password = ""
+
+all_characters = letters + numbers + symbols
+
 #for loop to create password
 for i in range(pass_len):
-    random_letter = random.choice(letters)
-    password = password + random_letter
-    random_numbers = random.choice(numbers)
-    password = password + random_numbers
-    random_symbols = random.choice(symbols)
-    password = password + random_symbols
+    random_char = random.choice(all_characters)
+    password = password + random_char
 print(password)
