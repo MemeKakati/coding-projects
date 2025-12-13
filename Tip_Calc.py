df36135d2840b18ef5af945b00cfe6099789e30d
Meme <memekakati@gmail.com>
2025-12-12 22:59:36 -0500

    Updating fuctions, working with conditional functions

--- Tip_Calc.py
+++ Tip_Calc.py
@@ -1,10 +1,10 @@
-def tip(bill_amount, tip_percentage):
+def tip(bill_amount, tip_percentage=20):
     return bill_amount * (tip_percentage / 100)
 
-result = tip(50, 20)
+result = tip(50)
 print(result)  # Example usage: should return 10.0  
 
-def total_with_tip(bill_amount, tip_percentage):
+def total_with_tip(bill_amount, tip_percentage=20):
     return bill_amount + tip(bill_amount, tip_percentage)
-result2 = total_with_tip(50,20)
+result2 = total_with_tip(50)
 print(result2)
